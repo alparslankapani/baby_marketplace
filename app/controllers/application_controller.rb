54cf9614f4eebd148affd90e88724a6c32dce211
Alparslan Kapani <alparslankapani@gmail.com>
2014-08-18 20:06:07 +0300

    Deploy  Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
     devise_parameter_sanitizer.for(:sign_up) << :image
-    devise_parameter_sanitizer.for(:sign_in) << :image
     devise_parameter_sanitizer.for(:account_update) << :image
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,12 @@ Rails.application.routes.draw do
       resources :orders, only: [:create, :new]
     end
 
-    get 'pages/about'
-    get 'pages/contact'
+    match 'about' => 'pages#about', via: [:get]
+    get  'contact' => 'pages#contact'
     get 'seller' => "listings#seller"
     get 'sales' => "orders#sales"
     get 'purchases' => "orders#purchases"
-
-
+    
     root 'listings#index'
   end
 
