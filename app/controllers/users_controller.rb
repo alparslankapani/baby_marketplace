77f7d5d1e2245a18f1c530d6af986c09711bba08
Alparslan Kapani <alparslankapani@gmail.com>
2014-09-18 14:48:05 +0300

    Update new review form with clickable star ratings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     @listings = @user.listings 
-    @reviews = Review.where(user_id: @user.id)
+    @reviews = Review.where(user_id: @user.id).order("id DESC")
+    if @reviews.blank?
+    	@avg_rating = 0 
+    else
+    	@avg_rating = @reviews.average(:rating).round(2)
+	end
   end
 end
 
