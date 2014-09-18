64804d292fa239ecf6ba14dfab70eab238c19170
Alparslan Kapani <alparslankapani@gmail.com>
2014-09-18 15:59:15 +0300

    Add validation to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ActiveRecord::Base
 	belongs_to :user
+
+	validates :rating, :comment, presence: true
+	validates :rating, numericality: {
+		only_integer: true,
+		greater_then_or_equal_to: 1,
+		less_than_or_equal_to: 5,
+		message: "Değerlendirme sadece 1 ile 5 arasında ve tam sayı olarak yapılır."
+	}
+
 end
