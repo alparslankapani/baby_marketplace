75be61636b491f1cc869407da231020710ad93f4
Alparslan Kapani <alparslankapani@gmail.com>
2014-09-23 18:19:13 +0300

    flike button integration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "alparslankapani@webiesel.com"
+  default from: "Karlı Bebek <alparslankapani@webiesel.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
